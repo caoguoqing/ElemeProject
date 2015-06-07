2efccd47b4ad17545a4d44e51764958bd71cd804
Sam Lau <liuyaozhu13hao@163.com>
2015-06-07 21:32:59 +0800

    Create ElemeApp project

---
+++ ElemeApp/ElemeApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ElemeApp
+//
+//  Created by Sam Lau on 6/7/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
