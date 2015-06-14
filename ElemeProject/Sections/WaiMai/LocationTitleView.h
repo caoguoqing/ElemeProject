9394c437b05a8a70adcab9b58b9c4f2ef8039222
Sam Lau <liuyaozhu13hao@163.com>
2015-06-14 15:02:49 +0800

    Create LocationTitleView class

---
+++ ElemeProject/Sections/WaiMai/LocationTitleView.h
@@ -0,0 +1,13 @@
+//
+//  LocationTitleView.h
+//  
+//
+//  Created by Sam Lau on 6/14/15.
+//
+//
+
+#import "SHPAbstractView.h"
+
+@interface LocationTitleView : SHPAbstractView
+
+@end
---
+++ ElemeProject/Sections/WaiMai/LocationTitleView.m
@@ -0,0 +1,14 @@
+//
+//  LocationTitleView.m
+//  
+//
+//  Created by Sam Lau on 6/14/15.
+//
+//
+
+#import "LocationTitleView.h"
+
+@implementation LocationTitleView
+
+
+@end
